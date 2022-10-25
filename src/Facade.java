ad1786ea64e2cbe0ca1b140e9d09ffeed0ffb990
Dylan-B-Johnson <dmaxbear123@gmail.com>
2022-10-24 21:46:11 -0400

    added to UI + refactoring

--- src/Facade.java
+++ src/Facade.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
-import java.util.UUID;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Facade {
 
@@ -91,6 +91,16 @@ public class Facade {
 
     public boolean addActivity(Activity activity) {
         return false;
+    }
+
+    public String[] nextWeek() {
+        String[] rtn = new String[7];
+        LocalDate date = LocalDate.now();
+        for (int i = 0; i < rtn.length; i++) {
+            LocalDate plusDays = date.plusDays(i);
+            rtn[i] = (plusDays.format(DateTimeFormatter.ofPattern("E, LLL d")));
+        }
+        return rtn;
     }
 
 }
--- src/UI.java
+++ src/UI.java
@@ -7,22 +7,65 @@ public class UI {
         run();
     }
 
-    public static void run(){
-        while (true){
-            if (welcomeScreen()){
-                f.setUser(login());
+    private static void run() {
+        while (true) {
+            if (f.getUser() == null) {
+                if (welcomeScreen()) {
+                    login();
+                } else {
+                    createAccount();
+                }
             }
-            else{
-                f.setUser(createAccount());
-            }
-            if (f.getUser()!=null){
-                // select appropriate user screen
+            if (f.getUser() != null) {
+                switch (f.getUser().getTypeOfUser()) {
+                    case COUNSELOR:
+                        if (counselorScreen()) {
+                            viewSchedule();
+                        } else {
+                            viewGroup();
+                        }
+                        break;
+                    case CUSTOMER:
+                        // select appropriate user screen
+
+                        break;
+                    // select appropriate user screen
+
+                    case DIRECTOR:
+                        // select appropriate user screen
+
+                }
             }
 
         }
     }
 
-    public static boolean welcomeScreen() {
+    private static void viewSchedule() {
+        while (true) {
+            title("View Schedule");
+            int answer=options(f.nextWeek());
+
+        }
+    }
+
+    private static void viewGroup() {
+
+    }
+
+    private static boolean counselorScreen() {
+        while (true) {
+            title("Welcome " + f.getUser().getFirstName() + "!");
+            switch (options(new String[] { "View Schedule", "View Group" })) {
+                case 1:
+                    return true;
+                case 2:
+                    return false;
+            }
+
+        }
+    }
+
+    private static boolean welcomeScreen() {
         while (true) {
             title("Welcome to " + f.getCampLocation().getName());
             print("Located at " + f.getCampLocation().getLocation() + ", and managed by "
@@ -33,49 +76,42 @@ public class UI {
                 print(i.getName());
             }
             print("");
-            String answer = input("Please select one of the following options by entering the coresponding number:\n"
-                    + "(1) Login\n(2) Create Account");
-            Scanner scan = new Scanner(answer);
-            if (answer.equalsIgnoreCase("login")) {
-                login();
-            }
-            if (answer.equalsIgnoreCase("create account")) {
-                createAccount();
-            }
-            try {
-                if (scan.nextInt() == 1) {
-                    scan.close();
+            switch (options(new String[] { "Login", "Create Account" })) {
+                case 1:
                     return true;
-                }
-                if (scan.nextInt() == 2) {
-                    scan.close();
+                case 2:
                     return false;
-                }
-            } catch (Exception e) {
-                basicError(answer);
             }
-            scan.close();
 
         }
 
     }
 
-    public static User createAccount() {
+    private static void createAccount() {
         title("Create Your Account");
-        while (true){
-            
+        f.setUser(f.signUp(input("Please enter your email address:"),
+                input("Please enter a password for your account:")));
+        if (f.getUser() == null) {
+            title("ERROR");
+            input("Your email or password were invalid. Please try again.\n(Press enter to continue).");
         }
     }
 
-    public static User login() {
-
+    private static void login() {
+        title("Login");
+        f.setUser(f.login(input("Please enter your email address:"),
+                input("Please enter a password for your account:")));
+        if (f.getUser() == null) {
+            title("ERROR");
+            input("Your email or password were invalid. Please try again.\n(Press enter to continue).");
+        }
     }
 
-    public static void print(String string) {
+    private static void print(String string) {
         System.out.println(string);
     }
 
-    public static String input(String prompt) {
+    private static String input(String prompt) {
         print(prompt);
         Scanner scan = new Scanner(System.in);
         String answer = scan.nextLine();
@@ -83,20 +119,53 @@ public class UI {
         return answer;
     }
 
-    public static void cls() {
+    private static void cls() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
 
-    public static void basicError(String answer) {
+    private static void basicError(String answer) {
         title("ERROR");
-        input(answer+ "\" is not a valid option, please try again.\n(Press enter to continue).");
+        input(answer + "\" is not a valid option, please try again.\n(Press enter to continue).");
         cls();
     }
 
-    public static void title(String title) {
+    private static void title(String title) {
         cls();
         print("***** " + title + " *****");
     }
 
+    private static int options(String[] options) {
+        String optionsString = "";
+        for (int i = 0; i < options.length; i++) {
+            if (i < options.length - 1) {
+                optionsString += "(" + (i + 1) + ") " + options[i] + "\n";
+            } else {
+                optionsString += "(" + (i + 1) + ") " + options[i];
+            }
+        }
+        String answer = input(
+                "Please select one of the following options by entering the coresponding number:\n" + optionsString);
+        for (int i = 0; i < options.length; i++) {
+            if (answer.equalsIgnoreCase(options[i])) {
+                return i + 1;
+            }
+        }
+        Scanner scan = new Scanner(answer);
+        int optionNum;
+        try {
+            optionNum = scan.nextInt();
+        } catch (Exception e) {
+            scan.close();
+            basicError(answer);
+            return -1;
+        }
+        if (optionNum < 1 || optionNum > options.length) {
+            scan.close();
+            basicError(answer);
+            return -1;
+        }
+        scan.close();
+        return optionNum;
+    }
 }
