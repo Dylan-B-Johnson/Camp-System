c788286824e054c68114439a76f7ab3eea65a234
Collin Remer <ctremer@email.sc.edu>
2022-10-31 02:22:30 -0400

    Added Counselor JavaDoc

--- src/Counselor.java
+++ src/Counselor.java
@@ -1,9 +1,12 @@
-// Copyright 2022 Row 3
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.UUID;
 
+/**
+ * A counselor
+ * 
+ * @author Row 3
+ */
 public class Counselor extends User {
     private ArrayList<String> allergies;
     private LocalDate birthday;
@@ -11,6 +14,20 @@ public class Counselor extends User {
     private Contact secondaryEmergencyContact;
     private Contact primaryCarePhysician;
 
+    /**
+     * Creates a counselor given the following parameters
+     * 
+     * @param allergies
+     * @param birthday
+     * @param email
+     * @param firstName
+     * @param lastName
+     * @param password
+     * @param campLocation
+     * @param primaryEmergencyContact
+     * @param secondaryEmergencyContact
+     * @param primaryCarePhysician
+     */
     public Counselor(ArrayList<String> allergies, LocalDate birthday, String email, String firstName,
             String lastName, String password,
             CampLocation campLocation, Contact primaryEmergencyContact, Contact secondaryEmergencyContact,
@@ -23,6 +40,21 @@ public class Counselor extends User {
         this.primaryCarePhysician = primaryCarePhysician;
     }
 
+    /**
+     * Creates a counselor given the following parameters, including ID
+     * 
+     * @param id
+     * @param allergies
+     * @param birthday
+     * @param email
+     * @param firstName
+     * @param lastName
+     * @param password
+     * @param campLocation
+     * @param primaryEmergencyContact
+     * @param secondaryEmergencyContact
+     * @param primaryCarePhysician
+     */
     public Counselor(UUID id, ArrayList<String> allergies, LocalDate birthday, String email,
             String firstName, String lastName, String password,
             CampLocation campLocation, Contact primaryEmergencyContact, Contact secondaryEmergencyContact,
@@ -35,56 +67,111 @@ public class Counselor extends User {
         this.primaryCarePhysician = primaryCarePhysician;
     }
 
+    /**
+     * Passes a String ArrayList, if not empty, sets as Counselors allergies
+     * 
+     * @param allergies
+     */
     public void setAllergies(ArrayList<String> allergies) {
         if (allergies != null) {
             this.allergies = allergies;
         }
     }
 
+    /**
+     * Gets the counselors allergies
+     * 
+     * @return Counselors allergies
+     */
     public ArrayList<String> getAllergies() {
         return this.allergies;
     }
 
+    /**
+     * Passes a LocalDate, if not empty, sets as Counselors birthday
+     */
     public void setBirthday(LocalDate birthday) {
         if (birthday != null) {
             this.birthday = birthday;
         }
     }
 
+    /**
+     * Gets counselors birthday
+     * 
+     * @return Counselors birthday
+     */
     public LocalDate getBirthday() {
         return this.birthday;
     }
 
+    /**
+     * Passes through a Contact, if not empty, sets as counselors primary emergency
+     * contact
+     * 
+     * @param primaryEmergencyContact
+     */
     public void setPrimaryEmergencyContact(Contact primaryEmergencyContact) {
         if (primaryEmergencyContact != null) {
             this.primaryEmergencyContact = primaryEmergencyContact;
         }
     }
 
+    /**
+     * Gets specified counselors primary emergency contact
+     * 
+     * @return Counselors primary emergency contact
+     */
     public Contact getPrimaryEmergencyContact() {
         return this.primaryEmergencyContact;
     }
 
+    /**
+     * Passes through a contact, if not empty, sets as counselors second emergency
+     * contact
+     * 
+     * @param secondaryEmergencyContact
+     */
     public void setSecondaryEmergencyContact(Contact secondaryEmergencyContact) {
         if (secondaryEmergencyContact != null) {
             this.secondaryEmergencyContact = secondaryEmergencyContact;
         }
     }
 
+    /**
+     * Gets counselors secondary emergency contact
+     * 
+     * @return Counselors secondary emergency contact
+     */
     public Contact getSecondaryEmergencyContact() {
         return this.secondaryEmergencyContact;
     }
 
+    /**
+     * Passes through a Contact, if not empty, sets as Counselors PCP
+     * 
+     * @param primaryCarePhysician
+     */
     public void setPrimaryCarePhysician(Contact primaryCarePhysician) {
         if (primaryCarePhysician != null) {
             this.primaryCarePhysician = primaryCarePhysician;
         }
     }
 
+    /**
+     * Gets specified counselors PCP
+     * 
+     * @return Counselors PCP
+     */
     public Contact getPrimaryCarePhysician() {
         return this.primaryCarePhysician;
     }
 
+    /**
+     * Creates a string representation of counselors attributes
+     * 
+     * @return String representation of counselors attributes
+     */
     public String toString() {
         return "Counselor Name: " + this.firstName + " " + this.lastName +
                 "\nAllergies: " + this.allergies.toString() +
