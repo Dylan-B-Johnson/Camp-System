dfdaa1e719dd7b3da0d3bb88f68774dd14679f9b
Collin Remer <ctremer@email.sc.edu>
2022-10-30 22:30:40 -0400

    Added JavaDoc

--- src/User.java
+++ src/User.java
@@ -1,7 +1,10 @@
-// Copyright 2022 Row 3
-
 import java.util.UUID;
 
+/**
+ * A user
+ * 
+ * @author Row 3
+ */
 public abstract class User {
     protected UUID id;
     protected String email;
@@ -11,6 +14,16 @@ public abstract class User {
     protected CampLocation campLocation;
     protected TypeOfUser typeOfUser;
 
+    /**
+     * Creates a User given the following parameters
+     * 
+     * @param email
+     * @param firstName
+     * @param lastName
+     * @param password
+     * @param campLocation
+     * @param typeOfUser
+     */
     public User(String email, String firstName, String lastName, String password, CampLocation campLocation,
             TypeOfUser typeOfUser) {
         this.id = UUID.randomUUID();
@@ -22,6 +35,17 @@ public abstract class User {
         this.typeOfUser = typeOfUser;
     }
 
+    /**
+     * Creates a User given the following parameters, which includes ID
+     * 
+     * @param id
+     * @param email
+     * @param firstName
+     * @param lastName
+     * @param password
+     * @param campLocation
+     * @param typeOfUser
+     */
     public User(UUID id, String email, String firstName, String lastName, String password, CampLocation campLocation,
             TypeOfUser typeOfUser) {
         this.id = id;
@@ -33,71 +57,147 @@ public abstract class User {
         this.typeOfUser = typeOfUser;
     }
 
+    /**
+     * Creates a String representation of characteristics for users
+     * 
+     * @return String with characteristics of users
+     */
     public String toString() {
         return "Name: " + firstName + " " + lastName + "\n" +
                 "Email: " + email + "\n" +
                 "Type of User: " + typeOfUser;
     }
 
+    /**
+     * gets the users ID
+     * 
+     * @return users ID
+     */
     public UUID getId() {
         return this.id;
     }
 
+    /**
+     * gets the users email
+     * 
+     * @return users email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Checks to make sure the email being entered has a valid format
+     * 
+     * @param email
+     * @return true if email is valid, false if it is not
+     */
     public static boolean emailIsValid(String email) {
         return email != null && email.contains("@") && email.contains(".") && email.length() >= 3
                 && UserList.emailAvailable(email);
     }
 
+    /**
+     * If email is valid, sets the email for user
+     * 
+     * @param email
+     */
     public void setEmail(String email) {
         if (email != null && email.contains("@") && email.contains(".") && email.length() >= 3
                 && UserList.emailAvailable(email))
             this.email = email;
     }
 
+    /**
+     * Gets the first name of the user
+     * 
+     * @return users first name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Takes in a String and sets it to users firstName
+     * 
+     * @param firstName
+     */
     public void setFirstName(String firstName) {
         if (firstName != null)
             this.firstName = firstName;
     }
 
+    /**
+     * Gets the last name of the user
+     * 
+     * @return users last name
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * takes in a String and sets it to users lastName
+     * 
+     * @param lastName
+     */
     public void setLastName(String lastName) {
         if (lastName != null)
             this.lastName = lastName;
     }
 
+    /**
+     * Gets the users password
+     * 
+     * @return users password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Takes in a String and sets it to the users password
+     * 
+     * @param password
+     */
     public void setPassword(String password) {
         if (password != null)
             this.password = password;
     }
 
+    /**
+     * Gets the current campLocation
+     * 
+     * @return campLocation
+     */
     public CampLocation getCampLocation() {
         return campLocation;
     }
 
+    /**
+     * If campLocation is not empty, sets it to the user
+     * 
+     * @param campLocation
+     */
     public void setCampLocation(CampLocation campLocation) {
         if (campLocation != null)
             this.campLocation = campLocation;
     }
 
+    /**
+     * Returns specific type of user
+     * 
+     * @return type of user: Counselor, Director, Customer
+     */
     public TypeOfUser getTypeOfUser() {
         return typeOfUser;
     }
 
+    /**
+     * Sets the type of user
+     * 
+     * @param typeOfUser
+     */
     public void setTypeOfUser(TypeOfUser typeOfUser) {
         this.typeOfUser = typeOfUser;
     }
--- src/UserList.java
+++ src/UserList.java
@@ -1,10 +1,19 @@
-
-//Copyright Row 3
 import java.util.ArrayList;
 import java.util.UUID;
 
+/**
+ * A list of Users
+ * 
+ * @author Row 3
+ */
 public class UserList {
 
+    /**
+     * Checks to see if the email entered is available for use
+     * 
+     * @param email
+     * @return True if email is available, false if it is not
+     */
     public static boolean emailAvailable(String email) {
         for (User user : DataReader.getUsers().values()) {
             if (user.getEmail().equals(email)) {
@@ -14,6 +23,11 @@ public class UserList {
         return true;
     }
 
+    /**
+     * Checks which type of user it is, then adds them to the system
+     * 
+     * @param user
+     */
     public static void addUser(User user) {
         switch (user.getTypeOfUser()) {
             case DIRECTOR:
@@ -28,26 +42,58 @@ public class UserList {
         }
     }
 
-    public static ArrayList<User> getUsers(){
+    /**
+     * Gets a list of all users
+     * 
+     * @return ArrayList of all users currently in the system
+     */
+    public static ArrayList<User> getUsers() {
         return new ArrayList<User>(DataReader.getUsers().values());
     }
 
+    /**
+     * Gets a list of all customers
+     * 
+     * @return ArrayList of all customers currently in the system
+     */
     public static ArrayList<Customer> getCustomers() {
         return new ArrayList<Customer>(DataReader.getCustomers().values());
     }
 
+    /**
+     * Gets a list of all counselors
+     * 
+     * @return ArrayList of all counselors currently in the system
+     */
     public static ArrayList<Counselor> getCounselors() {
         return new ArrayList<Counselor>(DataReader.getCounselors().values());
     }
 
+    /**
+     * Gets a list of all campers
+     * 
+     * @return ArrayList of all campers currently in the system
+     */
     public static ArrayList<Camper> getCampers() {
         return new ArrayList<Camper>(DataReader.getCampers().values());
     }
 
+    /**
+     * Gets the director
+     * 
+     * @return The director of the camp
+     */
     public static Director getDirector() {
         return DataReader.getDirector();
     }
 
+    /**
+     * Checks to see if the ID entered is valid, then gets a specific user based on
+     * the ID entered
+     * 
+     * @param id
+     * @return User associated with the ID
+     */
     public static User getUser(UUID id) {
         if (!DataReader.getUsers().containsKey(id))
             return null;
