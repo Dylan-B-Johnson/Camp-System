eefdc931c917e96d7b482493a23cc70f3fccdaa4
Dylan-B-Johnson <dmaxbear123@gmail.com>
2022-11-10 14:27:52 -0500

    started Facade testing

---
+++ src/FacadeTesting.java
@@ -0,0 +1,22 @@
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FacadeTesting{
+    @BeforeEach
+    public void setup(){
+        // Read in all used JSONs to String
+        // Empty all used JSONs 
+        // Add in testing data
+    }
+
+    @AfterEach
+    public void tearDown(){
+        //Save all used JSONs to String as they were
+    }
+
+    
+}
+
