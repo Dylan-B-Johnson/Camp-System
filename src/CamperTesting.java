803233ad3aee220360971d240b8c984725da7a20
hughesandrewb <hughesandrewb@gmail.com>
2022-11-13 21:48:20 -0500

    changes

--- src/CamperTesting.java
+++ src/CamperTesting.java
@@ -19,51 +19,51 @@ public class CamperTesting {
     private Contact primaryCarePhysician;
 
     @BeforeEach
-    public void setup(){
-    primaryEmergencyContact = new Contact("Ryan", "Harp", "harr@gmail.com",
-            "657-234-2231", "Friend",
-            "55 Hoback St.");
-    secondaryEmergencyContact = new Contact("Thomas", "Mandrus", "mandrusT@gmail.com",
-            "456-586-2231", "Friend",
-            "76 Hoback St.");
-    primaryCarePhysician = new Contact("Ron", "Rhymer", "ron@pak.org", "811-234-2333",
-            "PCP",
-            "Pediatric Association of Knoxbville, 23 College Drive");
-    allergies = new ArrayList<String>();    
+    public void setup() {
+        primaryEmergencyContact = new Contact("Ryan", "Harp", "harr@gmail.com",
+                "657-234-2231", "Friend",
+                "55 Hoback St.");
+        secondaryEmergencyContact = new Contact("Thomas", "Mandrus", "mandrusT@gmail.com",
+                "456-586-2231", "Friend",
+                "76 Hoback St.");
+        primaryCarePhysician = new Contact("Ron", "Rhymer", "ron@pak.org", "811-234-2333",
+                "PCP",
+                "Pediatric Association of Knoxbville, 23 College Drive");
+        allergies = new ArrayList<String>();
     }
-    
+
     @AfterEach
-    public void tearDown(){
+    public void tearDown() {
 
     }
 
     @Test
-    public void getNonexistentParent(){
-        Camper testCamper = new Camper("caleb", "test", allergies, LocalDate.now().minusYears(12), 
-            primaryEmergencyContact, secondaryEmergencyContact, primaryCarePhysician, 0, " ", " ");  
+    public void getNonexistentParent() {
+        Camper testCamper = new Camper("caleb", "test", allergies, LocalDate.now().minusYears(12),
+                primaryEmergencyContact, secondaryEmergencyContact, primaryCarePhysician, 0, " ", " ");
         assertNull(testCamper.getParent());
     }
 
     @Test
-    public void getExistingParent(){
-        Customer testCustomer = new Customer("email", "hey", "there", "null", 
-            CampLocation.getCampLocation(), new ArrayList<Camper>(), primaryEmergencyContact);
-        Camper testCamper = new Camper("caleb", "test", allergies, LocalDate.now().minusYears(12), 
-            primaryEmergencyContact, secondaryEmergencyContact, primaryCarePhysician, 0, " ", " ");  
+    public void getExistingParent() {
+        Customer testCustomer = new Customer("email", "hey", "there", "null",
+                CampLocation.getCampLocation(), new ArrayList<Camper>(), primaryEmergencyContact);
+        Camper testCamper = new Camper("caleb", "test", allergies, LocalDate.now().minusYears(12),
+                primaryEmergencyContact, secondaryEmergencyContact, primaryCarePhysician, 0, " ", " ");
         testCustomer.addCamper(testCamper);
         DataWriter.createCamper(testCamper);
         UserList.addUser(testCustomer);
-        
+
         assertEquals(testCustomer, testCamper.getParent());
     }
 
     @Test
-    public void checkUnregisteredCamper(){
-        //TODO
+    public void checkUnregisteredCamper() {
+        // TODO
     }
-    
+
     @Test
-    public void checkRegisteredCamper(){
+    public void checkRegisteredCamper() {
 
     }
 }
